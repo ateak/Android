31bf381282e3131834e23ef863af8432b60b82dd
ateak <cerebus@student.21-school.ru>
2023-04-18 21:02:05 +0300

    Cats can say different phrases. Project NatashaAndCats finished.

--- lesson4/NatashaAndCats/app/src/main/java/ru/alexanderklimov/natashaandcats/MainActivity.java
+++ lesson4/NatashaAndCats/app/src/main/java/ru/alexanderklimov/natashaandcats/MainActivity.java
@@ -3,6 +3,11 @@ package ru.alexanderklimov.natashaandcats;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView secondTextView = findViewById(R.id.second_text);
+        TextView thirdTextView = findViewById(R.id.third_text);
+        TextView fourTextView = findViewById(R.id.fourth_text);
+
+        ImageView right_bottom_image = findViewById(R.id.right_bottom_image);
+        right_bottom_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] phrases = {"Уже 6 часов утра, Наташ",
+                        "Вставай, мы там все уронили",
+                        "Мы уронили вообще все, Наташ, честно",
+                        "Наташ, ты че опять лежишь?",
+                        "Часики-то тикают",
+                        "Мужика-то хоть нашла себе?",
+                        "Уже дитачек пора рожать вообще-то"};
+
+                shuffleArray(phrases); //перемешиваем
+
+                secondTextView.setText(phrases[0]);
+                thirdTextView.setText(phrases[1]);
+                fourTextView.setText(phrases[2]);
+            }
+        });
+    }
+
+    void shuffleArray(String[] arr) {
+        Random rnd = new Random();
+        for (int i = arr.length - 1; i > 0; i--) {
+            int index = rnd.nextInt(i + 1);
+            String a = arr[index];
+            arr[index] = arr[i];
+            arr[i] = a;
+        }
     }
 }
