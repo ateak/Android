d77ec8f0e018f4af9670078abaa072d8640e6600
ateak <cerebus@student.21-school.ru>
2023-04-20 22:07:26 +0300

    Создала третий экран, связала через кнопки два экрана с MainActivity + добавила EditText для передачи сообщения от одной Activity другой

--- lesson5/SeveralActivities/app/src/main/java/ru/alexanderklimov/severalactivities/MainActivity.java
+++ lesson5/SeveralActivities/app/src/main/java/ru/alexanderklimov/severalactivities/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +17,18 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClick(View view) {
         Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+        startActivity(intent);
+    }
+
+    public void onImageClick(View view) {
+        EditText userEditText = findViewById(R.id.editTextUser);
+        EditText giftEditText = findViewById(R.id.editTextGift);
+
+        Intent intent = new Intent(MainActivity.this, BirthdayActivity.class);
+
+        intent.putExtra("username", userEditText.getText().toString());
+        intent.putExtra("gift", giftEditText.getText().toString());
+
         startActivity(intent);
     }
 }
